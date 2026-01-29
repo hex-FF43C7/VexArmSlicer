b59aafb0c3acad1a340a408fd34ca55e32043ee0
hex-FF43C7 <125480741+hex-FF43C7@users.noreply.github.com>
2026-01-29 07:41:37 -0700

    Refactor ArmSlicer class and add end effector support

--- main.py
+++ main.py
@@ -3,13 +3,42 @@
 import math
 import random
 import time
+import regex
 
 class ArmSlicer:
   def __init__(self):
     self.gcode = []
+    self.pos = (0, 0, 0) # x y z
+    self.end_eff = ""
+
+    self.PEN = "PEN"
+    self.MAGNET = "MAGNET"
+
+    self.valid_ends = [self.PEN, self.MAGNET]
+
+  def set_end_effector_type(self, new_type):
+    if new_type is in self.valid_ends:
+      self.end_eff = new_type
+    else:
+      raise Exception(f"WTF IS DIS?? {new_type}")
 
   def get_gcode_str(self):
     return '\n'.join(self.gcode)
+
+  def get_gcode_commands(self, arm_object_name="arm", include_initilization=True):
+    init_template = \
+    f"""
+    import cte
+    {arm_object_name} = cte.arm()
+    """
+    answer = []
+
+    if include_initilization:
+      anser.append(init_template)
+
+    for command in self.gcode:
+      pass # PUT DECODE STUFF HERE
+    
 
   def write_to_file(self, file):
     file.write(self.get_gcode_str())
@@ -18,8 +47,9 @@ class ArmSlicer:
     # someday I'll work out what the bounds of the bot are, but for now just assume its okay
     return True
   
-  def move_to(self, x, y, z, w=0)
-    self.gcode.append(f"G0 X{x} Y{y} Z{z}")
+  def move_to(self, x=None, y=None, z=None, w=0)
+    cmd_tmp = f"X{self.pos[0] if x is None else x} Y{self.pos[1] if y is None else y} Z{self.pos[2] if z is None else z}"
+    self.gcode.append(f"G0 {cmd_tmp}")
     if w != 0:
       self.gcode.append(f"G4 {w.upper()}")
 
